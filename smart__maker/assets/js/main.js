6dac64e9c926734faf4abc94948c22d8e8caf3c7
seoplisuk <seoplisuk@gmail.com>
2023-01-30 08:30:54 +0200

    2

--- smart__maker/assets/js/main.js
+++ smart__maker/assets/js/main.js
@@ -183,3 +183,46 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 /* end */
 
+$(".block__hidden").hide();
+$(".block__header").click(function () {
+	
+	$(this).next().next().slideToggle();
+	$(this).parent().toggleClass('opened');
+});
+
+
+$(".success").click(function () {
+	$(this).toggleClass('success__open');
+});
+$(".wrong").click(function () {
+	$(this).toggleClass('wrong__open');
+});
+
+
+
+// получаем массив всех вкладок
+const tabs = document.querySelectorAll(".tab");
+// получаем массив всех блоков с содержимым вкладок
+const contents = document.querySelectorAll(".hidden__content1");
+ 
+// запускаем цикл для каждой вкладки и добавляем на неё событие
+for (let i = 0; i < tabs.length; i++) {
+	tabs[i].addEventListener("click", ( event ) => {
+ 
+		// сначала нам нужно удалить активный класс именно с вкладок
+		let tabsChildren = event.target.parentElement.children;
+		for (let t = 0; t < tabsChildren.length; t++) {
+			tabsChildren[t].classList.remove("tab--active");
+		}
+		// добавляем активный класс
+		tabs[i].classList.add("tab--active");
+		// теперь нужно удалить активный класс с блоков содержимого вкладок
+		let tabContentChildren = event.target.parentElement.nextElementSibling.children;
+		for (let c = 0; c < tabContentChildren.length; c++) {
+			tabContentChildren[c].classList.remove("content--active");
+		}
+		// добавляем активный класс
+		contents[i].classList.add("content--active");
+ 
+	});
+}
