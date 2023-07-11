9f228b5630ab64fc67b0f04908a61a146549d6d4
seoplisuk <seoplisuk@gmail.com>
2023-07-11 18:26:27 +0300

    2
    3

--- max/assets/js/main.js
+++ max/assets/js/main.js
@@ -36,6 +36,42 @@ if (document.documentElement.clientWidth < 1024) {
         
       });
 
+
+	  const swiper2 = new Swiper('.section7__swiper', {
+	
+        autoHeight: true,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+        
+      
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+    
+        breakpoints: {
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 0,
+              autoHeight: true,
+            },
+            
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 0
+            }
+          }
+      
+        
+        
+      });
+
 }
 
 
