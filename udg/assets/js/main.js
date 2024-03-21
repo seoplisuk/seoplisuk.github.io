645cec0240d9403b282fb6ac3f571738695a3561
Alexandr Plisyuk <seoplisuk@gmail.com>
2024-03-21 12:02:30 +0200

    123
    123

--- udg/assets/js/main.js
+++ udg/assets/js/main.js
@@ -189,6 +189,25 @@ jQuery(document).ready(function(){
 });
 
 
+$(document).ready(function(){
+    // = Вешаем событие прокрутки к нужному месту
+    	//	 на все ссылки якорь которых начинается на #
+    	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+    		e.preventDefault();
+    
+    		var target = this.hash,
+    		$target = $(target);
+    
+    		$('html, body').stop().animate({
+    			'scrollTop': $target.offset().top
+    		}, 900, 'swing', function () {
+    			window.location.hash = target;
+    		});
+    	});
+    
+    });
+
+
 
 
 
