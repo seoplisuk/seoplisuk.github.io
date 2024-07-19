8e6c27fdb4485d23ab2bfe80c0dbf81c84ca510d
Alexandr Plisyuk <seoplisuk@gmail.com>
2024-07-19 16:44:11 +0300

    123
    123

--- harbor/assets/js/main.js
+++ harbor/assets/js/main.js
@@ -257,3 +257,24 @@ window.addEventListener("DOMContentLoaded", function () {
     return new UltimateBlocksCounter(wrapper);
   });
 });
+
+
+
+$('#filter1').selectize({ normalize: true });
+
+
+
+var filter_select_el = document.getElementById('filter1');
+var items_el = document.getElementById('items');
+
+filter_select_el.onchange = function() {
+  console.log(this.value);
+  var items = items_el.getElementsByClassName('item');
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].classList.contains(this.value)) {
+      items[i].style.display = 'block';
+    } else {
+      items[i].style.display = 'none';
+    }
+  }
+}
